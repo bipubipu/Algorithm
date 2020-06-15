3154195f5f7681a047f7d0ea7a397b78512f519f
bipubipu <wyqatm@gmail.com>
2020-06-15 12:36:01 +1000

    Update Drop.js

--- Drop.js
+++ Drop.js
@@ -2,3 +2,10 @@
 function dropElements(arr, func) {
   return arr.slice(arr.findIndex(func) >= 0 ? arr.findIndex(func) : arr.length);
 }
+
+function dropElements(arr, func) {
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+}
