49441d6670127e165d1f5d17a4795977b65f4042
bipubipu <wyqatm@gmail.com>
2020-06-17 17:02:44 +1000

    Update addTogether.js

--- addTogether.js
+++ addTogether.js
@@ -1,12 +1,13 @@
 //Create a function that sums two arguments together. If only one argument is provided, then return a function that expects 
-one argument and returns the sum. If either argument isn't a valid number, return undefined.
+// one argument and returns the sum. If either argument isn't a valid number, return undefined.
 
 // This is the most difficult challenge for me now.
 
 function addTogether(first, second) {
   if (typeof first!= 'number')
     return undefined;
-  const sum = second =>
-    typeof second === "number" ? first + second : undefined;
-  return typeof second === "undefined" ? second => sum(second) : sum(second);
+  if (typeof second == "number") 
+    return first + second;
+  if (typeof second === "undefined") 
+    return result => addTogether(first, result);
 }
