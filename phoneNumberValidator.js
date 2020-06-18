31367383a1c4ca5f00b88bf14e1bb740bfb5aaae
bipubipu <wyqatm@gmail.com>
2020-06-18 15:58:37 +1000

    Improve regex

--- phoneNumberValidator.js
+++ phoneNumberValidator.js
@@ -7,7 +7,7 @@
 1 555 555 5555
 */
 function telephoneCheck(str) {
-    const reg= /^1?\s?-?\s?(\d{3}|\(\d{3}\))\s?-?\d{3}-?\s?\d{4}$/;
+    const reg= /^1?[\s-\s]?(\d{3}|\(\d{3}\))[\s-]?\d{3}[-\s]?\d{4}$/;
   return reg.test(str);
 }
 // The trickest thing is to write regex for a pair of parentheses. I googled and found '\(([^()]*)\)' can match any characters
